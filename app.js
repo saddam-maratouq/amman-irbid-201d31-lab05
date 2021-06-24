fb982bd789ea169079083ebb4ab978c402213131
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-06-24 17:06:08 +0300

    creat p2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function sum(a, b) {
 
 }
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7); 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -32,11 +32,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  //eslint-disable-line
-}
+  let sum1= a ; 
+  let sum2= b ; 
+  let sum= sum1 * sum2 ;
+  let messges= `The product of ${sum1} and ${sum2} is ${sum}.`
+  return [sum,messges]
+
+} 
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
